dfde5fdf9b50abbf2b55092e3245672f79ec153e
Admin <admin@Admins-MacBook-Pro.local>
2024-08-19 21:32:35 +0530

    Misc

--- src/main/java/com/techlearning/Application.java
+++ src/main/java/com/techlearning/Application.java
@@ -6,9 +6,10 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-//@OpenAPIDefinition(info = @Info(title = "Demo API", version = "2.0", description = "Swagger API Demo"))
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
