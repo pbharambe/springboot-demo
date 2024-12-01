b96df5b4551f75a97ea7ab1412432300326d68b0
Admin <admin@Admins-MacBook-Pro.local>
2024-12-01 21:02:50 +0530

    Added ConfigurationProperties change

---
+++ src/main/java/com/techlearning/property/AppProperties.java
@@ -0,0 +1,13 @@
+package com.techlearning.property;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("app")
+public class AppProperties {
+
+    private String testProp;
+}
